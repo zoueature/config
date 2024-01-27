2a6df91609e881546f346752f80e85e647754814
wahaha <me@wahaha.com>
2024-01-27 11:26:59 +0800

    add grpc listen add in app config

--- app.go
+++ app.go
@@ -1,6 +1,8 @@
 package config
 
 type AppConfig struct {
-	Listen string `json:"listen" yaml:"listen"`
-	Debug  bool   `json:"debug" yaml:"debug"`
+	Listen     string `json:"listen" yaml:"listen"`
+	GrpcListen string `json:"grpcListen" yaml:"grpcListen"`
+	Debug      bool   `json:"debug" yaml:"debug"`
+	Env        string `json:"env" yaml:"env"`
 }
