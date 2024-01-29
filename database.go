7fac1b6f898e361c97c01afd314431b1e2b5033f
wahaha <me@wahaha.com>
2024-01-29 14:52:02 +0800

    add mongo config

--- database.go
+++ database.go
@@ -1,6 +1,9 @@
 package config
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type DatabaseType string
 
@@ -11,36 +14,59 @@ const (
 
 type DatabaseConfig struct {
 	Type     DatabaseType `json:"type" yaml:"type"`
-	Host     string       `json:"host" yaml:"host"`
-	Port     string       `json:"port" yaml:"port"`
 	Username string       `json:"username" yaml:"username"`
 	Password string       `json:"password" yaml:"password"`
 	Database string       `json:"database" yaml:"database"`
-	Charset  string       `json:"charset" yaml:"charset"`
-	Prefix   string       `json:"prefix" yaml:"prefix"`
-	Timezone string       `json:"timezone" yaml:"timezone"`
+	MinConn  *int         `json:"minConn" yaml:"minConn"`
+	MaxConn  *int         `json:"maxConn" yaml:"maxConn"`
+
+	Host     string `json:"host" yaml:"host"`
+	Port     string `json:"port" yaml:"port"`
+	Charset  string `json:"charset" yaml:"charset"`
+	Prefix   string `json:"prefix" yaml:"prefix"`
+	Timezone string `json:"timezone" yaml:"timezone"`
+
+	Nodes []string `json:"nodes" yaml:"nodes"`
 }
 
-const defaultCharset = "utf8mb4"
-
-func (d DatabaseConfig) Dsn() string {
+func (d *DatabaseConfig) initDefaultInEmptyConfig() {
 	if d.Charset == "" {
 		d.Charset = defaultCharset
 	}
+	minConn := defaultMinConn
+	if d.MinConn == nil {
+		d.MinConn = &minConn
+	}
+	maxConn := defaultMaxConn
+	if d.MaxConn == nil {
+		d.MaxConn = &maxConn
+	}
+}
+
+const (
+	defaultCharset = "utf8mb4"
+	defaultMinConn = 1
+	defaultMaxConn = 100
+)
+
+func (d DatabaseConfig) Dsn() string {
+	d.initDefaultInEmptyConfig()
 	if d.Database == "" {
 		panic("[Database Configuration] db name is empty")
 	}
 	s := ""
 	switch d.Type {
 	case mysqlType:
-		s = d.ToMySQLDsn()
+		s = d.mysqlDsn()
+	case mongoType:
+		s = d.toMongoDsn()
 	default:
 		panic(driverNotDefinedErr)
 	}
 	return s
 }
 
-func (d DatabaseConfig) ToMySQLDsn() string {
+func (d DatabaseConfig) mysqlDsn() string {
 	if d.Timezone == "" {
 		d.Timezone = "Local"
 	}
@@ -52,5 +78,21 @@ func (d DatabaseConfig) ToMySQLDsn() string {
 		d.Database,
 		d.Charset,
 		d.Timezone,
+	)
+}
+
+// mongodb://myDatabaseUser:D1fficultP%40ssw0rd@db0.example.com,db1.example.com,db2.example.com/?replicaSet=myRepl&w=majority&wtimeoutMS=5000
+func (d DatabaseConfig) toMongoDsn() string {
+	if len(d.Nodes) == 0 {
+		panic("[Database Configuration] mongo node is empty. ")
+	}
+	return fmt.Sprintf(
+		"mongodb://%s:%s@%s/%s?minPoolSize=%d&maxPoolSize=%d",
+		d.Username,
+		d.Password,
+		strings.Join(d.Nodes, ","),
+		d.Database,
+		d.MaxConn,
+		d.MinConn,
 	)
 }
