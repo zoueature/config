6ab48ccdb911aead5fdd3eb1620ae27e49e56261
zoueature <zoueature@gmail.com>
2024-03-01 10:13:33 +0800

    add kafka brokers config in mq config

--- mq.go
+++ mq.go
@@ -1,7 +1,12 @@
 package config
 
 type Mq struct {
-	Driver         string `json:"driver" yaml:"driver" desc:"消息队列类型, nsq, kafka, rabbit"`
-	ConsumeAddress string `json:"consumeAddress" yaml:"consumeAddress" desc:"消费者链接地址"`
-	ProductAddress string `json:"productAddress" yaml:"productAddress" desc:"生产者链接地址"`
+	Driver string `json:"driver" yaml:"driver" desc:"消息队列类型, nsq, kafka, rabbit"`
+
+	// nsq
+	ConsumeAddress string `json:"consumeAddress" yaml:"consumeAddress" desc:"nsq消费者链接地址"` // nsq
+	ProductAddress string `json:"productAddress" yaml:"productAddress" desc:"nsq生产者链接地址"` // nsq
+
+	// kafka
+	Brokers []string `json:"brokers" yaml:"brokers" desc:"kafka brokers"`
 }
